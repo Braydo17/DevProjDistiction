120e2becfd703388a7b097c43aa836092c0143f5
Luke Bayliss <lukepbayliss@gmail.com>
2016-05-11 19:43:40 +1000

    Adds Bootstrap Framework
    Adds the bootstrap framework to the project as well as a starting point
    for the index page. Also adds AngularJS as was part of the Framework I
    had prepared, whether it's used or not isn't a worry.

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <title>Template</title>
+    <!-- Bootstrap Styling -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="Container">
+        <h1>Template</h1>
+        <div class="row">
+            <div class="col-xs-12">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+
+    <!-- Bootstrap JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- AngularJS script -->
+    <script src="js/angular.min.js"></script>
+
+    <!-- Controllers (If angular/ JavaScript is used) -->
+</body>
+
+</html>
